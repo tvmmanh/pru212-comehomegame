60ef76388fec4f9dcbe2e902d001c5401c71fcf3
manh <tivip1216vn@gmail.com>
2024-05-27 22:12:18 +0700

    scene

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -38,6 +38,8 @@ public class Health : MonoBehaviour
         }
     }
 
+
+
     public void Healing(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);

c8f83d231b4f62ad589d7ec649fc79e81bb79a20
Leficios <khoatlse173181@fpt.edu.vn>
2024-05-28 19:34:36 +0700

    MainMenu

---
+++ Assets/Scripts/Main Menu/Play.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayButton()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
